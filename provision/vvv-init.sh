5f6718e3e8a450e800045acac96bfc86ab5531dc
Jussi Mäkinen <jussi@jussimakinen.fi>
2017-11-14 23:01:19 +0200

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -23,7 +23,7 @@ if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/index.php" ]]; then
   echo "Downloading Drupal..."
   cd "${VVV_PATH_TO_SITE}/public_html/"
   wget https://ftp.drupal.org/files/projects/drupal-8.4.2.tar.gz
-  tar -x -f drupal-8.4.2.tar.gz -v -z --strip-components=1
+ # tar -x -f drupal-8.4.2.tar.gz -v -z --strip-components=1
 fi
 
 cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
