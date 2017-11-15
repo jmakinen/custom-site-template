d5de025efb4e46ff31101b84faa4eb74dd1cf774
Jussi Mäkinen <jussi@jussimakinen.fi>
2017-11-15 07:59:54 +0200

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -21,7 +21,7 @@ touch ${VVV_PATH_TO_SITE}/log/access.log
 
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/index.php" ]]; then
   echo "Downloading Drupal..."
-  # cd "${VVV_PATH_TO_SITE}/public_html/"
+  cd ${VVV_PATH_TO_SITE}
   wget https://ftp.drupal.org/files/projects/drupal-8.4.2.tar.gz
   tar -x -f drupal-8.4.2.tar.gz -v -z 
   mv drupal-8.4.2 public_html
